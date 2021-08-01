be40724cb3f306d95b2814f95362600f5e78c1cb
Xcho <khchoavagyan@gmail.com>
2021-08-01 20:20:51 +0400

    Initial commit

---
+++ src/main/java/com/example/awsproject/CustomController.java
@@ -0,0 +1,14 @@
+package com.example.awsproject;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomController {
+
+    @GetMapping(value = "/home")
+    public ResponseEntity<String> create(){
+        return ResponseEntity.ok("pisik");
+    }
+}
