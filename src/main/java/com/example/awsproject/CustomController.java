b89f7d8580cb90eaa282d0ede00121d907fbc320
Xcho <khchoavagyan@gmail.com>
2021-08-16 22:37:40 +0400

    new

--- src/main/java/com/example/awsproject/CustomController.java
+++ src/main/java/com/example/awsproject/CustomController.java
@@ -9,6 +9,6 @@ public class CustomController {
 
     @GetMapping(value = "/home")
     public ResponseEntity<String> create(){
-        return ResponseEntity.ok("Barev pisik");
+        return ResponseEntity.ok("Barev pisik Lyovana");
     }
 }
