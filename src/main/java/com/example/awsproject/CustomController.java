b3930129fc619764acfcf97b589470b2041fbde8
Khachatur Avagyan <khchoavagyan@gmail.com>
2021-09-13 15:29:46 +0400

    new IP

--- src/main/java/com/example/awsproject/CustomController.java
+++ src/main/java/com/example/awsproject/CustomController.java
@@ -2,13 +2,16 @@ package com.example.awsproject;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomController {
 
-    @GetMapping(value = "/home")
-    public ResponseEntity<String> create(){
-        return ResponseEntity.ok("Maybe by ssh is disconnected");
+    @GetMapping(value = "/home/{text}")
+
+    public ResponseEntity<String> create(@PathVariable String text ){
+        text +=  "you write " + text;
+        return ResponseEntity.ok(text);
     }
 }
